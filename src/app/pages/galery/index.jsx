c692f75405708760168d525059984f0fbf773235
Gabo Desktop <gabo5612@gmail.com>
2023-10-10 10:24:30 -0400

    galery

--- src/app/pages/galery/index.jsx
+++ src/app/pages/galery/index.jsx
@@ -4,21 +4,24 @@ import Image from 'next/image';
 import aboutMe from 'public/assets/aboutme.png'
 import aboutMe1 from 'public/assets/aboutme1.png'
 import aboutMe2 from 'public/assets/aboutme2.png'
-// Import Swiper styles
+
 import 'swiper/css';
+import 'swiper/css/pagination';
+import { Pagination } from 'swiper/modules';
 
 export default function Galery() {
   return (
     <Swiper
-      spaceBetween={20}
+      spaceBetween={5}
       slidesPerView={3}
+      pagination={true} modules={[Pagination]}
       onSlideChange={() => console.log('slide change')}
       onSwiper={(swiper) => console.log(swiper)}
-    >
-      <SwiperSlide className='w-full h-full'><Image src={aboutMe} alt='img'/></SwiperSlide>
-      <SwiperSlide className='w-full h-full'><Image src={aboutMe} alt='img'/></SwiperSlide>
-      <SwiperSlide className='w-full h-full'><Image src={aboutMe} alt='img'/></SwiperSlide>
-      <SwiperSlide className='w-full h-full'><Image src={aboutMe} alt='img'/></SwiperSlide>
+     className='bg-black pt-5 mySwiper'>
+      <SwiperSlide><Image className='w-full' src={aboutMe} alt='img'/></SwiperSlide>
+      <SwiperSlide><Image className='w-full' src={aboutMe} alt='img'/></SwiperSlide>
+      <SwiperSlide><Image className='w-full' src={aboutMe} alt='img'/></SwiperSlide>
+      <SwiperSlide><Image className='w-full' src={aboutMe} alt='img'/></SwiperSlide>
       ...
     </Swiper>
   );
