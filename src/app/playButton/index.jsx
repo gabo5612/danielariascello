3b6dd70ac2343c7d6a7270e613de6dda7484f70a
Gabo Desktop <gabo5612@gmail.com>
2023-10-23 15:25:54 -0400

    agregado el boton de play

--- src/app/playButton/index.jsx
+++ src/app/playButton/index.jsx
@@ -2,16 +2,29 @@
 import useSound from 'use-sound';
 import Image from 'next/image'
 import { useState } from 'react';
+
 export default function PlayButton() {
 
-    const [playbackRate, setPlaybackRate] = useState(0.75);
 
-    const [play, forceSoundEnabled] = useSound('/sounds/Faure.mp3', { playbackRate});
- 
+    const [play, {pause}] = useSound('/sounds/Faure.mp3');
+      const [isPlaying, setIsPlaying]=useState(false)
+   
+      const handlePlaying=()=>{
+        if(isPlaying){
+          pause()
+          setIsPlaying(!isPlaying)
+
+      
+        }else{
+          play()
+          setIsPlaying(!isPlaying)
+  
+        }
+      }
   return (
-    <div className='fixed bottom-6 right-6'>
-      <button className='sticky' onLoadStart={play()}>
-        <Image width='100' height='100' src='/assets/play.svg' alt='play' />
+    <div className='fixed bottom-6 right-4 z-50'>
+      <button className='sticky' onClick={()=> handlePlaying()} >
+        <Image width='75' height='75' src={isPlaying? '/assets/pause.svg' : '/assets/play.svg'} alt='play' />
       </button>
     </div>
   );
