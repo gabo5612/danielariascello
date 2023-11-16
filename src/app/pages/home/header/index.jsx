7c43081f754f0a223187793579aaf7e529074265
Gabo Desktop <gabo5612@gmail.com>
2023-11-16 11:41:14 -0400

    video comprimido

--- src/app/pages/home/header/index.jsx
+++ src/app/pages/home/header/index.jsx
@@ -10,7 +10,7 @@ export default function Header(){
     return (
         <div className="h-screen w-full " onLoad={()=>context.setIsHome(true)}>
             <video  autoPlay={true} loop muted className=' w-full h-full object-cover z-0'> 
-                <source src='/assets/Video.mp4' type='video/mp4'/>
+                <source src='public/assets/Video.mp4' type='video/mp4'/>
                 <div className=' w-full h-full  flex justify-center items-center flex-col absolute top-0 z-10'>
                     <div className='w-10/10'>
                     <h1 className="text-5xl text-center text-white font-cinzel text-shadow-md drop-shadow-xl custom-shadow-text ">Daniel Arias</h1>
