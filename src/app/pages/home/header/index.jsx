d84cb1d008a80846c65d8503b124133e72b63290
Gabo Desktop <gabo5612@gmail.com>
2023-11-16 11:03:08 -0400

    listo el video

--- src/app/pages/home/header/index.jsx
+++ src/app/pages/home/header/index.jsx
@@ -9,18 +9,19 @@ export default function Header(){
     const context=useContext(Context)
     return (
         <div className="h-screen w-full " onLoad={()=>context.setIsHome(true)}>
-            <video  autoPlay={true} loop muted className=' w-full h-full object-cover'>
-                <source src='/assets/videoDesktopBg.mp4' type='video/mp4'/>
-                <div className='bg-dark/40 w-full h-full   flex justify-center items-center flex-col  z-10'>
+            <video  autoPlay={true} loop muted className=' w-full h-full object-cover'> </video>
+                <source src='/assets/video.mp4' type='video/mp4'/>
+                <div className=' w-full h-full  flex justify-center items-center flex-col absolute top-0 z-10'>
                     <div className='w-10/10'>
                     <h1 className="text-5xl text-center text-white font-cinzel text-shadow-md drop-shadow-xl custom-shadow-text ">Daniel Arias</h1>
                     <h2 className='text-end text-6xl text-shadow-md  font-corinthia text-secunday custom-shadow-text'>Cello</h2>
                     </div>
                    
                 </div>
+               
               <NavBar/>
                
-              </video>
+              
         </div>
     );
 }
